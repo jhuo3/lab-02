28ccadb3eb2c001af60990770c8c3b79db061df1
jhuo3 <55111004+jhuo3@users.noreply.github.com>
2019-09-15 18:29:32 -0400

    Delete nav.php

--- lab01/inc/nav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-	<ul>
-		<li><a href="index.php">Home</a></li>
-		<li><a href="early-days.php">Early Days</a></li>
-		<li><a href="work.php">Work Experience</a></li>
-		<li><a href="hobbies.php">Hobbies & Activities</a></li>
-	</ul>
-</nav>
